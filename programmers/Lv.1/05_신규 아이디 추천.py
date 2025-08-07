fa3eed88d13679a917af9af76358425f8dce6693
gkkary3 <nambawon1@naver.com>
2025-08-07 14:31:08 +0900

    Lv.1 문자열 처리 Finish Commit

--- programmers/Lv.1/05_신규 아이디 추천.py
+++ programmers/Lv.1/05_신규 아이디 추천.py
@@ -1,18 +1,64 @@
+import re
 # 아이디의 길이는 3자 이상 15자 이하
 # 아이디는 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.) 문자만 사용할 수 있습니다.
 # 단, 마침표(.)는 처음과 끝에 사용할 수 없으며 또한 연속으로 사용할 수 없습니다.
 def solution(new_id):
-    answer = ''
     new_id_low = ''
-
+    print('new_id: ', new_id)
     # 1단계
     for id in new_id:
         if id.isalpha():
             new_id_low += id.lower()
         else:
             new_id_low += id
-    print(new_id_low)
-    return answer
+    print('1단계: ', new_id_low)
+    # 2단계
+    new_id_low = re.sub(r'[^a-z0-9\-_.]', '', new_id_low)
+    print('2단계: ', new_id_low)
+
+    # 3단계
+    new_id_low = re.sub(r'\.+', '.', new_id_low)
+    print('3단계: ', new_id_low)
+
+    # 4단계
+    if new_id_low[0] == '.' or new_id_low[-1] == '.':
+        new_id_low = dotRemove(new_id_low)
+
+    # 간단한 방법(.strip('.'))
+    # : 문자열 양쪽 끝에 있는 마침표만 제거
+    print('4단계: ', new_id_low)
+
+    # 5단계
+    if new_id_low == "":
+        new_id_low = 'a'
+    print("5단계: ", len(new_id_low))
+    # 6단계
+    print("len: ", new_id_low)
+    if len(new_id_low) >= 16:
+        new_id_low = new_id_low[:15]
+        print("split_new_id: ", new_id_low)
+        if new_id_low[0] == '.' or new_id_low[-1] == '.':
+            new_id_low = dotRemove(new_id_low)
+    print("6단계: ", new_id_low)
+    # 7단계
+    if len(new_id_low) <= 2:
+        while len(new_id_low) < 3:
+            new_id_low =  new_id_low + new_id_low[-1]
+    print("7단계: ", new_id_low)
+    return new_id_low
+
+def dotRemove(new_id):
+    if len(new_id) > 0 and new_id[0] == '.':
+        new_id = new_id[1:]
+    print('dotRemove: ', new_id)
+    if len(new_id) > 0 and new_id[-1] == '.':
+        new_id = new_id[:-1]
+    return new_id
+
+# print(solution("...!@BaT#*..y.abcdefghijklm"))
+# print(solution("z-+.^."))
+# print(solution("=.="))
+print(solution("abcdefghijklmn.p"))
 
 # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
 # 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
